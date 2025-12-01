50ed0668e37b81b78e355e88b179dbc00e23c9b9
odinous95 <mjd.thif@icloud.com>
2025-12-01 03:57:26 -0800

    Refactor endpoints and improve error handling
    Refactored inline endpoint definitions into a dedicated method
    (`app.MapPlayerEndpoints()`) to enhance modularity and maintainability.
    Replaced `app.UseRouting()` and `app.UseEndpoints` with
    `app.UseExceptionHandler()` for centralized error handling. Removed
    unused `using` directives and added `LearningApp.Endpoints`. Introduced
    a conditional check for the development environment to apply specific
    configurations during development.

--- Program.cs
+++ Program.cs
@@ -1,40 +1,14 @@
-using LearningApp.Models;
-using Microsoft.AspNetCore.Mvc;
-using System.ComponentModel.DataAnnotations;
-
+using LearningApp.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 var app = builder.Build();
 
-// Middleware 
-app.UseRouting();
-
-
-app.UseEndpoints(static endpoints =>
+if (app.Environment.IsDevelopment())
 {
-    _ = endpoints.MapGet("/players/{id:int}", (int id) =>
-    {
-        Player player = PlayersRepositoryInMomoeryDB.GetPlayerById(id);
-        return player is not null
-            ? TypedResults.Ok(player)
-            : Results.ValidationProblem(new Dictionary<string, string[]>
-            {
-                { "id", new[] { $"Player with the id {id} does not exists" } }
-            });
-    });
-      
-    _ = endpoints.MapGet("/players", () =>
-    {
-        List<Player> players = PlayersRepositoryInMomoeryDB.GetPlayers();
-        return players is not null
-            ? TypedResults.Ok(players)
-            : Results.ValidationProblem(new Dictionary<string, string[]>
-        {
-                { "players", new[] { "Players do not exist!" } }
-    });
-});
-});
+    app.UseExceptionHandler();
+}
+
+app.MapPlayerEndpoints();
 
 app.Run();
