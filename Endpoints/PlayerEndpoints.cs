d3a697e35f119aff7487629b84ddad9141187983
odinous95 <mjd.thif@icloud.com>
2025-12-01 03:58:21 -0800

    Add PlayerEndpoints for player-related HTTP routes
    Introduced a new `PlayerEndpoints` static class to define and map
    HTTP endpoints for player operations. Added `/players/{id:int}`
    to fetch a player by ID and `/players` to retrieve all players.
    
    Implemented validation problem responses for cases where players
    or specific player IDs do not exist. Utilized
    `PlayersRepositoryInMomoeryDB` for data retrieval and used
    `TypedResults.Ok` and `Results.ValidationProblem` for response
    handling.

---
+++ Endpoints/PlayerEndpoints.cs
@@ -0,0 +1,34 @@
+﻿using LearningApp.Models;
+
+namespace LearningApp.Endpoints
+{
+    public static class PlayerEndpoints
+    {
+            public static void MapPlayerEndpoints(this WebApplication app)
+            {
+                  app.MapGet("/players/{id:int}", (int id) =>
+                  {
+                      Player player = PlayersRepositoryInMomoeryDB.GetPlayerById(id);
+                      return player is not null
+                          ? TypedResults.Ok(player)
+                          : Results.ValidationProblem(new Dictionary<string, string[]>
+                          {
+                { "id", new[] { $"Player with the id {id} does not exists" } }
+                          });
+                  });
+
+                app.MapGet("/players", () =>
+                {
+                    List<Player> players = PlayersRepositoryInMomoeryDB.GetPlayers();
+                    return players is not null
+                        ? TypedResults.Ok(players)
+                        : Results.ValidationProblem(new Dictionary<string, string[]>
+                    {
+                { "players", new [] { "Players do not exist!" } }
+                        });
+                });
+            }
+    }
+}
+
+
